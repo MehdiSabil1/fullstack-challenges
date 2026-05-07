8f54c13d1cfa32e4223b781d7659d63bdbf2e6c3
Alexander Dubovoy <ajdubovoy@gmail.com>
2026-05-07 12:45:32 +0200

    Move to 00

--- 01-Ruby/04-Hash-Symbols/00-On-hashes/lib/hashes.rb
+++ 01-Ruby/04-Hash-Symbols/00-On-hashes/lib/hashes.rb
@@ -11,27 +11,22 @@ def are_hash_keys_unique?
   # e.g. { "a" => 1, "a" => 2 } — does "a" appear twice?
   # TODO: Return true if keys are unique (second assignment wins), false otherwise
   # NOTE: You should just return true or false. It's a quiz question.
-  true
 end
 
 # Remember, docs are your friend
 # - https://ruby-doc.org/3.3.5/Hash.html
 def access_value(fruits)
   # TODO: return the value stored at the key "strawberry" 🍓 in the given hash `fruits`
-  fruits["strawberry"]
 end
 
 def add_key_value(fruits)
   # TODO: set the value of the key "mango" 🥭 to "delicious"
-  fruits["mango"] = "delicious"
 end
 
 def update_value(fruits)
   # TODO: update the value of the key "grape" 🍇 to "sour"
-  fruits["grape"] = "sour"
 end
 
 def remove_key(fruits)
   # TODO: remove the key "banana" 🍌
-  fruits.delete("banana")
 end

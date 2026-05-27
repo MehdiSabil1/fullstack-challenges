c2916cc75cc1d3f58ab60bd809e45c1831e789c5
mehdi sabil <mehdi.sabil@hotmail.com>
2026-05-27 19:11:13 +0200

    Added your stupid coach method and interface

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,7 +1,22 @@
 def coach_answer(your_message)
-  # TODO: return coach answer to your_message
+  if your_message == "I am going to work right now!"
+    return ""
+  elsif your_message.end_with?("?")
+    return "Silly question, get dressed and go to work!"
+  else
+    return "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours!
+  going_to_work = ["I am going to work right now!", "I AM GOING TO WORK RIGHT NOW!"]
+  return "" if going_to_work.include?(your_message)
+
+  prefix = your_message == your_message.upcase ? "I can feel your motivation! " : ""
+
+  if your_message.end_with?("?")
+    "#{prefix}Silly question, get dressed and go to work!"
+  else
+    "#{prefix}I don't care, get dressed and go to work!"
+  end
 end
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,14 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+
+puts "Hello, I'm your coach. What do you want to say to me?"
+
+message = ""
+going_to_work = ["I am going to work right now!", "I AM GOING TO WORK RIGHT NOW!"]
+
+until going_to_work.include?(message)
+  print "You: "
+  message = gets.chomp
+  puts "Coach: #{coach_answer_enhanced(message)}"
+end
+Ce qui a changé :

e8ad75695b4eedd46a0df945546be9be0e74bc1d
mehdi sabil <mehdi.sabil@hotmail.com>
2026-05-27 19:28:28 +0200

    Added colorful numbers method

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,3 +1,15 @@
 def colorful?(number)
   # TODO: return true if the number is colorful, false otherwise
+  digits = number.to_s.chars.map(&:to_i)
+  products = Set.new
+
+  (1..digits.length).each do |length|
+    digits.each_cons(length) do |combination|
+      product = combination.reduce(1, :*)
+      return false if products.include?(product)
+      products.add(product)
+    end
+  end
+
+  true
 end

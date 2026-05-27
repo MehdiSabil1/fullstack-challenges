02181f18c3141c8168df40b6ee6ed2830a39d692
mehdi sabil <mehdi.sabil@hotmail.com>
2026-05-27 13:57:02 +0200

    Add what's your name method

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,7 +1,15 @@
 require_relative "compute_name"
 
 # TODO: ask for the first name
+puts "Please enter your first name:"
+first_name = gets.chomp
 # TODO: ask for the middle name
+puts "Please enter your middle name:"
+middle_name = gets.chomp
 # TODO: ask for the last name
+puts "Please enter your last name:"
+last_name = gets.chomp
 # TODO: Print a greeting to the user with the full name concatenated,
+
+puts "Hello, #{compute_name(first_name, middle_name, last_name)}"
 #       something like "Hello, Boris Paillard"

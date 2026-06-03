7dc5264227102b05c5c82254ccad58ca191b45a8
mehdi sabil <mehdi.sabil@hotmail.com>
2026-06-03 23:11:51 +0200

    Add sort method and interface for user to add names

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -4,8 +4,38 @@ require_relative "wagon_sort"
 #       Remember, to read an input from the command line, use:
 #       - `gets`:  https://ruby-doc.org/3.3.5/Kernel.html#method-i-gets
 #       - `chomp`: https://ruby-doc.org/3.3.5/String.html#method-i-chomp
+students  = []
+name      = nil
 
+while name != ""
+  puts students.empty? ? "Type a student name:" : "Type another student name (or press enter to finish):"
+  name = gets.chomp
+
+  students << name if name != ""
+end
+
+sorted_students = wagon_sort(students)
+num_students = sorted_students.size
 
 
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
+#         should be case-insensitive when sorting (FAILED - 1)
+
+
+puts wagon_sort(students)
+
+def pluralize(number, word)
+  if number == 1
+    word
+  else
+    "#{word}s"
+  end
+end
+
+puts "Congratulations! Your Wagon has #{num_students} #{pluralize(num_students, 'student')}:"
+if sorted_students.size >= 2
+  puts "#{sorted_students[0..-2].join(', ')} and #{sorted_students.last}"
+else
+  puts sorted_students.first
+end
--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,5 @@
 def wagon_sort(students)
   # TODO: return (not print!) a copy of students, sorted alphabetically
+  #   should be case-insensitive when sorting (FAILED - 1)
+  students.sort_by(&:downcase)
 end

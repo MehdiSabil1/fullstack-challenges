1bf7b90813af39aacb15967cedaf0186968fa05e
mehdi sabil <mehdi.sabil@hotmail.com>
2026-06-03 22:06:30 +0200

    Add two methods for sum betwen min and max

--- 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
@@ -1,7 +1,26 @@
 def sum_with_for(min, max)
   # CONSTRAINT: you should use a for..end structure
+  if min > max
+    return -1
+  else
+    sum = 0
+    for i in min..max
+      sum += i
+    end
+    return sum
+  end
 end
 
 def sum_with_while(min, max)
   # CONSTRAINT: you should use a while..end structure
+  if min > max
+    return -1
+  else
+    while min <= max
+    sum ||= 0
+    sum += min
+    min += 1
+    end
+    return sum
+  end
 end

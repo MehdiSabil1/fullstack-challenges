76d3461849a3aea2b3a5b4f884da4b340a41ad5c
mehdi sabil <mehdi.sabil@hotmail.com>
2026-06-15 18:54:45 +0200

    added palindrome method

--- 01-Ruby/03-Iterators-Blocks/06-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Iterators-Blocks/06-Playing-with-strings/lib/palindrome.rb
@@ -1,3 +1,6 @@
 def palindrome?(a_string)
   # TODO: check if a_string is a palindrome
-end
+  cleaned = a_string.downcase.gsub(/[^a-z]/, "")
+  return false if cleaned.empty?
+  cleaned == cleaned.reverse
+  end
